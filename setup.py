34c95f891f2d1ea807f17aa68dc8310e269767ee
drdr xp <drdr.xp@gmail.com>
2020-12-22 21:36:58 +0800

    release: v0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name="md2zhihu",
     packages=["md2zhihu"],
-    version="0.1.1",
+    version="0.1.2",
     license='MIT',
     description='convert markdown to zhihu compatible format.',
     long_description='# md2zhihu\nConvert markdown to zhihu compatible format\n',
