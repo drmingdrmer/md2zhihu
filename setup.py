35c4c33c92126ae311f5b84ffab87a71fc3500b0
drdr xp <drdr.xp@gmail.com>
2020-12-22 21:36:18 +0800

    release: v0.1.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
 
     entry_points = {
         'console_scripts': [
-            'command-name = md2zhihu:main',                  
+            'md2zhihu = md2zhihu:main',                  
         ],              
     },
 
