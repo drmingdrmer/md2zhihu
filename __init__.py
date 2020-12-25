c0311fe6e075544cdd72f5b29269f7e7d54205a5
drdr xp <drdr.xp@gmail.com>
2020-12-25 15:13:27 +0800

    bump ver: 0.1.10

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ package-name is utility to create sub process.
 
 """
 
-__version__ = "0.1.9"
+__version__ = "0.1.10"
 __name__ = "md2zhihu"
 
 from . import md2zhihu
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
               "md2zhihu.mistune",
               "md2zhihu.md2zhihu",
               "md2zhihu.mistune.plugins"],
-    version="0.1.9",
+    version="0.1.10",
     license='MIT',
     description='convert markdown to zhihu compatible format.',
     long_description='# md2zhihu\n\n将markdown转换成 知乎 兼容的 markdown 格式\n\n## Install\n\n```sh\npip install md2zhihu\n```\n\n## Usage\n\n```sh\nmd2zhihu your_great_work.md\n```\n\n这个命令将markdown 转换成 知乎 文章编辑器可直接导入的格式, 存储到 `_md2/your_great_work/your_great_work.md`.\n`-o` 选项可以用来调整输出目录.\n\n## Features\n\n- 公式转换:\n\n  例如\n\n  ```\n  $$\n  ||X{\\vec {\\beta }}-Y||^{2}\n  $$\n  ```\n\n  $$\n  ||X{\\vec {\\beta }}-Y||^{2}\n  $$\n\n  转换成可以直接被知乎使用的tex渲染引擎的引用:\n\n  ```\n  <img src="https://www.zhihu.com/equation?tex=%7C%7CX%7B%5Cvec%20%7B%5Cbeta%20%7D%7D-Y%7C%7C%5E%7B2%7D%5C%5C" alt="||X{\\vec {\\beta }}-Y||^{2}\\\\" class="ee_img tr_noresize" eeimg="1">\n  ```\n\n  md2zhihu 能自动识别block的公式和inline的公式.\n\n- 表格: 将markdown表格转换成html 以便支持知乎直接导入.\n\n- 图片: md2zhihu 将图片上传到github, 并将markdown中的图片引用做替换.\n\n  - 默认命令例如`md2zhihu your_great_work.md`要求当前工作目录是一个git(作者假设用户用git来保存自己的工作), md2zhihu将建立一个随机分支来保存所有图片.\n\n  - 也可以使用指定的git repo来保存图片, 例如:\n\n    `md2zhihu your_great_work.md -r https://github.com/openacid/openacid.github.io.git `要求是对这个repo有push权限.\n\n## Limitation\n\n- 知乎的表格不支持table cell 中的markdown格式, 例如表格中的超链接, 无法渲染, 会被知乎转成纯文本.\n',
