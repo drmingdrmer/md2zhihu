208a7f783a7aafceb3385dba7e1552c603080ff1
drdr xp <drdr.xp@gmail.com>
2021-03-04 10:34:36 +0800

    force to use python3

--- github-install.sh
+++ github-install.sh
@@ -3,7 +3,7 @@
 npm install @mermaid-js/mermaid-cli@8.8.4
 sudo apt-get install pandoc
 
-pip install setuptools wheel
+pip3 install setuptools wheel
 cp md2zhihu/setup.py .
-python setup.py sdist bdist_wheel
-pip install dist/*.tar.gz
+python3 setup.py sdist bdist_wheel
+pip3 install dist/*.tar.gz
