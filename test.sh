79049bd476e497c7c3d0e35f022fa70b577c937b
drdr xp <drdr.xp@gmail.com>
2021-03-05 00:25:48 +0800

    rename install.sh to test.sh

--- test.sh
+++ test.sh
@@ -5,9 +5,7 @@ pip uninstall -y md2zhihu
 cp setup.py ..
 (
 cd ..
-rm dist/*
-python setup.py sdist bdist_wheel
-pip install dist/*.tar.gz
+python setup.py install
 )
 
 PYTHONPATH="$(cd ..; pwd)" pytest -x
