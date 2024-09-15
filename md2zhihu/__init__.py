29582a79d98c574fa2570a3998bd062e0b4a1cea
张炎泼 <drdr.xp@gmail.com>
2024-09-15 22:36:53 +0800

    refactor: render_with_features()

--- md2zhihu/__init__.py
+++ md2zhihu/__init__.py
@@ -1202,28 +1202,26 @@ def rules_to_features(rules):
 
 
 #  features: {typ:action(), typ2:{subtyp:action()}}
-def render_with_features(mdrender, rnode, features=None):
+def render_with_features(mdrender, rnode: RenderNode, features=dict) -> Optional[List[str]]:
     n = rnode.node
 
-    typ = n['type']
+    node_type = n['type']
 
-    f = features
-
-    if typ not in f:
+    if node_type not in features:
         return None
 
-    f = f[typ]
-    if callable(f):
-        return f(mdrender, rnode)
+    type_handler = features[node_type]
+    if callable(type_handler):
+        return type_handler(mdrender, rnode)
 
-    #  subtype is info
+    #  subtype is info, the type after "```"
     lang = n['info'] or ''
 
-    if lang in f:
-        return f[lang](mdrender, rnode)
+    if lang in type_handler:
+        return type_handler[lang](mdrender, rnode)
 
-    if '*' in f:
-        return f['*'](mdrender, rnode)
+    if '*' in type_handler:
+        return type_handler['*'](mdrender, rnode)
 
     return None
 
