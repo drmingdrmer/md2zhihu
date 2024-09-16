b42db0a374a507a645ebfda6f4b6672e688c17da
张炎泼 <drdr.xp@gmail.com>
2024-09-16 20:43:10 +0800

    fix: chunks() export list item as paragraph

--- test.sh
+++ test.sh
@@ -13,3 +13,4 @@ python setup.py install
 # PYTHONPATH="$(cd ..; pwd)" pytest -x -v --show-capture=all -k test_minimal_mistake
 # PYTHONPATH="$(cd ..; pwd)" pytest -x -v --show-capture=all -k test_weibo
 PYTHONPATH="$(cd ..; pwd)" pytest -x -v --show-capture=all -k test_chunks
+# PYTHONPATH="$(cd ..; pwd)" pytest -x -v --show-capture=all -k test_chunks_list
