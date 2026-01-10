487ba27d46f83834094a679c182d510d820a873a
Zhang Yanpo <drdr.xp@gmail.com>
2026-01-11 02:14:38 +0800

    chore: verbose test for CI

--- test.sh
+++ test.sh
@@ -3,9 +3,9 @@
 pip uninstall -y md2zhihu
 pip install -e ".[test]"
 
-# PYTHONPATH="$(cd ..; pwd)" pytest -x -v
+PYTHONPATH="$(cd ..; pwd)" pytest -x -v
 # PYTHONPATH="$(cd ..; pwd)" pytest -x -v -k test_simple
 # PYTHONPATH="$(cd ..; pwd)" pytest -x -v --show-capture=all -k test_minimal_mistake
 # PYTHONPATH="$(cd ..; pwd)" pytest -x -v --show-capture=all -k test_weibo
-PYTHONPATH="$(cd ..; pwd)" pytest -x -v --show-capture=all -k test_rules_to_features
+# PYTHONPATH="$(cd ..; pwd)" pytest -x -v --show-capture=all -k test_rules_to_features
 # PYTHONPATH="$(cd ..; pwd)" pytest -x -v --show-capture=all -k test_chunks_list
